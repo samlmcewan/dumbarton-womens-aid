6d77176e2536b87e574b66894d4e6fdbc48b0df6
samlmcewan <samlmcewan@gmail.com>
2024-12-06 17:01:40 +0000

    finish

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,31 +1,35 @@
-// const swiper1 = new Swiper('.swiper1', {
-//   // Optional parameters
-//   direction: 'horizontal',
-//   loop: true,
-//   autoplay: {
-//     delay: 5000, 
-//     disableOnInteraction: false,
-//   },
-//   pagination: {
-//     el: '.swiper-pagination',
-//     type: 'bullets',
-//   },
-//   slidesPerView: 1,
-//   spaceBetween: 20,
-//   breakpoints: {
-//     768: {
-//       slidesPerView: 3
+const swiper1 = new Swiper('.swiper1', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 5000, 
+    disableOnInteraction: false,
+  },
+  // pagination: {
+  //   el: '.swiper-pagination',
+  //   type: 'bullets',
+  // },
+  navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    768: {
+      slidesPerView: 2
       
-//     },
-//     992: {
-//       slidesPerView: 4
+    },
+    992: {
+      slidesPerView: 3
       
-//     },
-//     1200: {
-//       slidesPerView: 5
-//     }
-//   }
-// });
+    },
+    1200: {
+      slidesPerView: 3
+    }
+  }
+});
 
 // const swiper2 = new Swiper('.swiper2', {
 //   // Optional parameters
